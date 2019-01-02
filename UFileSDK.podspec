3958569a8134b4e4adebedd3420be91172ccff7f
ethan.zhang <ethan.zhang@ucloud.cn>
2019-01-02 10:53:56 +0800

    build 3.0.3

--- UFileSDK.podspec
+++ UFileSDK.podspec
@@ -16,7 +16,7 @@ Pod::Spec.new do |s|
   #
 
   s.name         = "UFileSDK"
-  s.version      = "3.0.2"
+  s.version      = "3.0.3"
   s.summary      = "UCloud Object Storage SDK for iOS"
 
   # This description is used to generate tags and improve search results.
--- UFileSDK/UFileSDK/UFileSDKConst.h
+++ UFileSDK/UFileSDK/UFileSDKConst.h
@@ -47,7 +47,7 @@ static const int KUFInvalidElements = -3; // instance array or dictionary error
 #define  KUFileSDKDownloadData   @"UFDownloadData"
 #define  KUFileSDKDownloadDestinationPath  @"UFDownloadDestinationPath"
 
-#define  KUFileSDKVersion  @"3.0.2"
+#define  KUFileSDKVersion  @"3.0.3"
 
 
 /****    for UFMultiPartInfo  ****/
